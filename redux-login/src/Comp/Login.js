9f5c9b9ab98ea87ed65f3f5f5f3a5c12062871cf
amisha26 <amisha8299@gmail.com>
2021-10-25 08:47:25 +0530

    redux-login login.js

--- redux-login/src/Comp/Login.js
+++ redux-login/src/Comp/Login.js
@@ -1,14 +1,31 @@
 import React, { useState } from "react";
-import './Login.css'
+import "./Login.css";
+import { useDispatch } from "react-redux";
 
 const Login = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    dispatch(
+      login({
+        name: name,
+        email: email,
+        password,
+        password,
+        loggedIn: true,
+      })
+    );
+  };
+
   return (
     <div className="login">
-      <form className="login_form">
+      <form className="login_form" onSubmit={(e) => handleSubmit(e)}>
         <h1>Login Here 🔐</h1>
         <input
           type="name"
@@ -28,7 +45,9 @@ const Login = () => {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <button type="submit" className="submit_btn">Submit</button>
+        <button type="submit" className="submit_btn">
+          Submit
+        </button>
       </form>
     </div>
   );
